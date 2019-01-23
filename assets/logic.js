8dec556857de566e00f8af7e8ac309b3db982944
Art G <theartofnoise@gmail.com>
2019-01-23 11:14:41 -0500

    added headers and such

---
+++ assets/logic.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBO73bDXdkPmcKxOPJiQX7l_uVv3KZPUjY",
+    authDomain: "rps-multiplayer-7443e.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-7443e.firebaseio.com",
+    projectId: "rps-multiplayer-7443e",
+    storageBucket: "rps-multiplayer-7443e.appspot.com",
+    messagingSenderId: "251723798578"
+  };
+  firebase.initializeApp(config);
+
+  var db = firebase.database().ref();
+
+
+
+
+
+});
